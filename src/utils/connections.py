5da7de80d7a55dea8ce1888ccaa57cac77e4e18a
Xixolio <ignacio.araya.11@alumnos.usm.cl>
2022-09-06 14:41:39 -0300

    added code to automatically create the db if deleted

--- src/utils/connections.py
+++ src/utils/connections.py
@@ -1,4 +1,7 @@
+import os
 import sqlite3
+
+BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 
 
 def get_connection(path, db_name):
@@ -11,5 +14,15 @@ def get_connection(path, db_name):
     :param db_name: DB name
     :return: sqlite3 connector object connected to the DB
     """
-    conn = sqlite3.connect(f'{path}{db_name}')
+    if not os.path.exists(f'{path}{db_name}'):
+        conn = sqlite3.connect(f'{path}{db_name}')
+        query_1 = open(f'{BASE_PATH}/../sql_queries/create_table_weather_daily_data.sql').read()
+        query_2 = open(f'{BASE_PATH}/../sql_queries/create_table_weather_hourly_data.sql').read()
+
+        conn.cursor().execute(query_1)
+        conn.cursor().execute(query_2)
+
+    else:
+        conn = sqlite3.connect(f'{path}{db_name}')
+
     return conn
